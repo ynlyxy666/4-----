88f11d19a8414150d3b66f68ce04c44be424ad4b
ynlyxy666 <3446271536@qq.com>
2025-01-20 19:49:07 +0800

    版本3

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import time
 from lib.CenterWindow import center_window as cw
 
 def bt1c():
-    bt1.config(text='只能打开一次的哦')
-    bt1.config(state='disabled')
+    #bt1.config(text='只能打开一次的哦')
+    #bt1.config(state='disabled')
     form2=tk.Toplevel(form1)
     form2.title('时间')
     cw(form2,640,360)
@@ -14,7 +14,7 @@ def bt1c():
     lb1=ttk.Label(form2,text=lb1t,font=('Cascadia Code',10))
     lb1.pack(padx=30,pady=30)
 
-def bt2c():
+def About():
     form3=tk.Toplevel(form1)
     form3.title('关于')
     lb2=ttk.Label(form3,text='关于界面',font=('Cascadia Code',10))
@@ -27,10 +27,15 @@ if __name__ == '__main__':
     #form1.geometry('640x360')
     form1.resizable(False,False)
     cw(form1,640,360)
+    menu1=tk.Menu(form1)
+    form1.config(menu=menu1)
+    menu1.add_command(label='文件')
+    menu1.add_command(label='帮助')
+    menu1.add_command(label='关于',command=About)
     main_title=ttk.Label(form1,text='主界面',font=('Cascadia Code',20))
     main_title.grid(row=0,column=0)
     bt1=ttk.Button(form1,text='打开一个时间界面',command=bt1c)
     bt1.grid(row=1,column=0)
-    bt2=ttk.Button(form1,text='打开一个关于界面',command=bt2c)
-    bt2.grid(row=2,column=0)
+    #bt2=ttk.Button(form1,text='打开一个关于界面',command=bt2c)
+    #bt2.grid(row=2,column=0)
     form1.mainloop()
