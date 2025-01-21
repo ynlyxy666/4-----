061429caa331750c3e45b2e33c17ed8134dfcf0e
ynlyxy666 <3446271536@qq.com>
2025-01-21 09:39:15 +0800

    2025.1.21 9.40

---
+++ lib/info.py
@@ -0,0 +1,2 @@
+about='关于\n版本：1.0.0\n作者：姜皓元，陆天睿\n版权：姜皓元\n侵权必究\n作者邮箱：3446271536@qq.com'
+help=open('lib/help.dat','r',encoding='utf-8').read()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter.scrolledtext as st
 from lib.StartupMovie import run
 import time
 from lib.CenterWindow import center_window as cw
+import lib.info as info
 import sys
 
 def bt1c():
@@ -19,20 +20,17 @@ def bt1c():
 def About():
     form3=tk.Toplevel(form1)
     form3.title('关于')
-    msg='关于\n版本：1.0.0\n作者：姜皓元，陆天睿\n版权：姜皓元\n侵权必究\n作者邮箱：3446271536@qq.com'
-    msgbx=tk.Message(form3,text=msg,font=('Cascadia Code',10),width=300)
+    msgbx=tk.Message(form3,text=info.about,font=('Cascadia Code',10),width=300)
     msgbx.pack(padx=30,pady=30)
     form3.resizable(False,False)
     cw(form3,360,220)
     
 def help():
-    fp=open('help.dat','r',encoding='utf-8')
-    helptext=fp.read()
     form4=tk.Toplevel(form1)
     form4.title('帮助')
     ht=st.ScrolledText(form4)
     ht.config(state=tk.NORMAL)
-    ht.insert(tk.END,helptext)
+    ht.insert(tk.END,info.help)
     ht.config(state=tk.DISABLED)
     ht.pack()
     form4.resizable(False,False)
@@ -55,8 +53,8 @@ if __name__ == '__main__':
     menu1.add_cascade(label='文件',menu=menu1_1)
     menu1.add_command(label='帮助',command=help)
     menu1.add_command(label='关于',command=About)
-    main_title=ttk.Label(form1,text='主界面',font=('Cascadia Code',20))
+    main_title=ttk.Label(form1,text='主界面',font=('华文彩云',20))
     main_title.grid(row=0,column=0)
-    bt1=ttk.Button(form1,text='打开一个时间界面',command=bt1c)
+    bt1=ttk.Button(form1,text='打开一个界面',command=bt1c)
     bt1.grid(row=1,column=0)
     form1.mainloop()
