cc9a8ef9e7650f7af4b579b555db3c9f8be7cadf
jhy <3446271536@qq.com>
2025-02-17 17:56:42 +0800

    2025.2.17 17.56

--- gui/start.py
+++ gui/start.py
@@ -25,22 +25,6 @@ def gui():
  
         return os.path.normpath(os.path.join(base_path, relative_path)) # 返回实际路径
 
-    def create_image():
-        # 加载自定义的PNG图标文件
-        img_path=get_path("tray.png")
-        image = Image.open(img_path)
-        return image
-
-    def bt1c():
-        #bt1.config(text='只能打开一次的哦')
-        #bt1.config(state='disabled')
-        form2=tk.Toplevel(form1)
-        form2.title('时间')
-        cw(form2,640,360)
-        lb1t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-        lb1=ttk.Label(form2,text=lb1t,font=('Cascadia Code',10))
-        lb1.pack(padx=30,pady=30)
-
     def About():
         form3=tk.Toplevel(form1)
         form3.title('关于')
@@ -65,20 +49,12 @@ def gui():
         form1.destroy()
         sys.exit()
 
-    def quit_window(icon: pystray.Icon): #type:ignore
-        icon.stop()
-        form1.destroy()
-
-    def show_window():
-        form1.deiconify()
-
-    def on_exit():
-        form1.withdraw()
-
     #print(sys.path)
     #run()
     form1=tk.Tk()
-    form1.title('主界面')
+    form1.title('课表生成')
+    style = ttk.Style()
+    style.theme_use('alt')
     form1.resizable(False,False)
     cw(form1,640,360)
     menu1=tk.Menu(form1,tearoff=False)
@@ -88,13 +64,7 @@ def gui():
     menu1.add_cascade(label='文件',menu=menu1_1)
     menu1.add_command(label='帮助',command=helptxt)
     menu1.add_command(label='关于',command=About)
-    main_title=ttk.Label(form1,text='主界面',font=('华文彩云',20))
-    main_title.grid(row=0,column=0)
-    #bt1=ttk.Button(form1,text='打开一个界面',command=bt1c)
-    #bt1.grid(row=1,column=0)
-    menu = (MenuItem('显示', show_window, default=True), Menu.SEPARATOR, MenuItem('退出', quit_window))
-    image = create_image()
-    icon = pystray.Icon("icon", image, "图标名称", menu)
-    form1.protocol('WM_DELETE_WINDOW', on_exit)
-    threading.Thread(target=icon.run, daemon=True).start()
+    bt1=ttk.Button(form1,text='开始',command=About)
+    bt1.pack(padx=4)
+    form1.protocol('WM_DELETE_WINDOW', quit)
     form1.mainloop()
---
+++ 打包.py
@@ -0,0 +1,25 @@
+import shutil
+import os
+
+# 假设这是你现有的代码
+os.system('pyinstaller -F -w --add-data "gif.gif;." main.pyw')
+os.remove('main.spec')
+
+# 补写的代码：删除 build 文件夹
+build_folder = 'build'
+if os.path.exists(build_folder) and os.path.isdir(build_folder):
+    shutil.rmtree(build_folder)
+    print(f"文件夹 {build_folder} 已删除")
+else:
+    print(f"文件夹 {build_folder} 不存在")
+
+dist_folder = 'dist'
+if os.path.exists(dist_folder) and os.path.isdir(dist_folder):
+    for filename in os.listdir(dist_folder):
+        if filename.endswith('.exe'):
+            src_file = os.path.join(dist_folder, filename)
+            dst_file = os.path.join(os.getcwd(), filename)
+            shutil.copy(src_file, dst_file)
+            print(f"文件 {filename} 已拷贝到当前目录")
+    shutil.rmtree(dist_folder)
+    print(f"文件夹 {dist_folder} 已删除")
